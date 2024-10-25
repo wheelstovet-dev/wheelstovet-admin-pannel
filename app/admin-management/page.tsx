ccad4818c8837cac0b859e07addb6b52f2170dce
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-10-25 08:33:54 +0530

    Added scrollarea to view all admins

--- app/admin-management/page.tsx
+++ app/admin-management/page.tsx
@@ -2,16 +2,19 @@
 import BreadCrumb from '@/components/breadcrumb';
 import MainLayout from '@/components/layout/main-layout';
 import { AdminManagementClient } from '@/components/tables/admin-management-tables/client';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 const breadcrumbItems = [{ title: 'Admin Management', link: '/dashboard/admin-management' }];
 
 export default function SubscriptionManagementPage() {
   return (
     <MainLayout meta={{ title: 'Admin Management' }}>
+      <ScrollArea className="h-full">
       <div className="flex-1 space-y-4 min-h-screen p-4 pt-6 md:p-8">
         <BreadCrumb items={breadcrumbItems} />
         <AdminManagementClient />
       </div>
+      </ScrollArea>
     </MainLayout>
   );
 }
