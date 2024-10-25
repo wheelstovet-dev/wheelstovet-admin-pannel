024633ceeb6ae91385a52fef7da3701daa854a6e
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-10-25 08:46:51 +0530

    updated getAllAdmin page with api integration

--- components/tables/admin-management-tables/client.tsx
+++ components/tables/admin-management-tables/client.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { DataTable } from '@/components/ui/data-table';
 import { Heading } from '@/components/ui/heading';
@@ -10,18 +10,56 @@ import { useRouter } from 'next/navigation';
 import { columns } from './columns';
 
 import { AdminManagement, AdminManagementData } from '@/constants/admin-management-data';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '@/app/redux/store';
+import { setLoading } from '@/app/redux/slices/authslice';
+import { getAllAdmin } from '@/app/redux/actions/adminAction';
+import { ToastAtTopRight } from '@/lib/sweetalert';
+
 
 export const AdminManagementClient: React.FC = () => {
   const router = useRouter();
-  const initialData: AdminManagement[] = AdminManagementData;
-  const [data, setData] = useState<AdminManagement[]>(initialData);
+  // const initialData: AdminManagement[] = AdminManagementData;
+  const [data, setData] = useState<any>([]);
+  const dispatch = useDispatch<AppDispatch>();
+  const [loader, setLoader] = useState(true);
 
   const handleSearch = (searchValue: string) => {
-    const filteredData = initialData.filter(item =>
-      item.fullName.toLowerCase().includes(searchValue.toLowerCase())
-    );
-    setData(filteredData);
+    // const filteredData = initialData.filter(item =>
+    //   item.fullName.toLowerCase().includes(searchValue.toLowerCase())
+    // );
+    // setData(filteredData);
   };
+
+  useEffect(() => {
+    getalladmin();
+  }, [])
+
+  const getalladmin = async () => {
+    dispatch(setLoading(true));
+    try {
+      const resultAction: any = await dispatch(getAllAdmin({ page: 1, limit: 20 })); // Dispatch the getAllAdmin action
+      // console.log(resultAction); 
+  
+      if (resultAction.type==='admin/getAll/fulfilled') {
+        
+        setData(resultAction?.payload?.data)
+        setLoader(false);
+        // console.log(resultAction.payload.data); 
+        
+      } else {
+        throw new Error(resultAction.payload?.message || 'Failed to fetch admins');
+      }
+    } catch (error: any) {
+      ToastAtTopRight.fire({
+        icon: 'error',
+        title: error.message || 'Failed to get admins',
+      });
+    } finally {
+      dispatch(setLoading(false));
+    }
+  };
+  
 
   // const handleSort = (sortBy: string, sortOrder: 'asc' | 'desc') => {
   //   // Example: Sorting by first name
@@ -56,6 +94,7 @@ export const AdminManagementClient: React.FC = () => {
         </Button>
       </div>
       <Separator />
+      {loader ? 'Loading...' :
       <DataTable
         // searchKeys="fullName"
         columns={columns}
@@ -64,6 +103,7 @@ export const AdminManagementClient: React.FC = () => {
         // filters={filters}
         // onSort={handleSort} 
       />
+}
     </>
   );
 };
