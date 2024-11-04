1ec34ff4797cc4dcfb182ca50cef70bec49c34b6
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-11-04 15:03:49 +0530

    action to get admin by id, update admin

--- app/redux/actions/adminAction.ts
+++ app/redux/actions/adminAction.ts
@@ -1,11 +1,11 @@
-// src/app/redux/actions/employeeActions.ts
+// src/app/redux/actions/adminActions.ts
 import apiCall from '@/lib/axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { AxiosResponse } from 'axios';
 
 export const createAdmin = createAsyncThunk<AxiosResponse<any>,any,{ rejectValue: any }>('admin/create', async (adminData, { rejectWithValue }) => {
   try {
-    // Make API call to create an employee
+    // Make API call to create an admin
     const response = await apiCall<any>(
       'POST',
       '/admin/createAdmin',
@@ -39,7 +39,7 @@ export const getAllAdmin = createAsyncThunk<
   }
 );
 
-// Action to get an employee by ID
+// Action to get an admin by ID
 export const getAdminById = createAsyncThunk<
   AxiosResponse<any>, // Return type is the entire Axios response
   string, // Input type of the admin ID as a string
@@ -48,8 +48,8 @@ export const getAdminById = createAsyncThunk<
   'admin/getById',
   async (AdminId, { rejectWithValue }) => {
     try {
-      // Make API call to get the employee by ID
-      const response = await apiCall('GET', `/admin/getAdmin${AdminId}`);
+      // Make API call to get the admin by ID
+      const response = await apiCall('GET', `/admin/getAdmin/${AdminId}`);
       // console.log('API Response:', response); // Log the full response for debugging
 
       // Return the entire response object
@@ -57,6 +57,44 @@ export const getAdminById = createAsyncThunk<
     } catch (error: any) {
       // Handle errors and return the error message
       return rejectWithValue(error || 'Failed to fetch admin credentials');
+    }
+  }
+);
+
+// Action to update an existing admin
+export const updateAdmin = createAsyncThunk<
+  AxiosResponse<any>,
+  { id: string; adminData: any }, // Input type includes admin ID and data
+  { rejectValue: any }
+>(
+  'admin/update',
+  async ({ id, adminData }, { rejectWithValue }) => {
+    try {
+      // console.log(adminData);
+      const response = await apiCall<any>('PUT', `/admin/${id}`, adminData);
+      // console.log('API Response:', response);
+      return response;
+    } catch (error: any) {
+      return rejectWithValue(error || 'Failed to update Admin');
+    }
+  }
+);
+
+//  Action to update an status of admin
+export const updateAdminStatus = createAsyncThunk<
+  AxiosResponse<any>,
+  { id: string; adminStatus: { IsActive: boolean } }, // Input type includes admin ID and status
+  { rejectValue: any }
+>(
+  'admin/updateStatus',
+  async ({ id, adminStatus }, { rejectWithValue }) => {
+    try {
+      // console.log(adminData);
+      const response = await apiCall<any>('PUT', `/admin/changeStatus/${id}`, adminStatus);
+      // console.log('API Response:', response);
+      return response;
+    } catch (error: any) {
+      return rejectWithValue(error || 'Failed to update Admin Status');
     }
   }
 );
