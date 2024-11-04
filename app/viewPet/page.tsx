94af1a51973ed008856b6132d9bf48abc1dfd96d
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-11-04 19:04:45 +0530

    added view pet

---
+++ app/viewPet/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+import React from 'react';
+import BreadCrumb from '@/components/breadcrumb';
+import MainLayout from '@/components/layout/main-layout';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { ViewPets } from '@/components/forms/view-pets/view-pets';
+
+const breadcrumbItems = [{ title: 'View', link: '/user-management/page.tsx' }];
+
+export default function View() {
+
+    return (
+        <MainLayout meta={{ title: 'View Pets' }}>
+            <ScrollArea className="h-full">
+                <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
+                    <BreadCrumb items={breadcrumbItems} />
+                    <ViewPets />
+                </div>
+            </ScrollArea>
+        </MainLayout>
+    );
+}
