c6cb02b777d4c93fc7edb62f65b58e5137e649ca
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-11-04 15:05:54 +0530

    added get employee by id , update employee actions

--- app/redux/slices/employee.ts
+++ app/redux/slices/employee.ts
@@ -1,6 +1,6 @@
 
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { createEmployee, getAllEmployees } from '../actions/employeeAction';
+import { createEmployee, getAllEmployees, getEmployeeById, updateEmployee } from '../actions/employeeAction';
 import { AxiosResponse } from 'axios';
 
 interface EmployeeState {
@@ -37,6 +37,7 @@ const employeeSlice = createSlice({
         state.loading = true;
       })
       .addCase(createEmployee.fulfilled, (state, action: PayloadAction<AxiosResponse<any>>) => {
+        // console.log('emp'+ action.payload.data);
         state.loading = false;
         const newEmployee = action.payload.data;
         state.employees.push(newEmployee);
@@ -45,17 +46,21 @@ const employeeSlice = createSlice({
         state.loading = false;
         state.error = action.payload;
       })
-    //   .addCase(getEmployeeById.pending, (state) => {
-    //     state.loading = true;
-    //   })
-    //   .addCase(getEmployeeById.fulfilled, (state, action: PayloadAction<AxiosResponse<Employee>>) => {
-    //     state.loading = false;
-    //     state.selectedEmployee = action.payload.data;
-    //   })
-    //   .addCase(getEmployeeById.rejected, (state, action: PayloadAction<any>) => {
-    //     state.loading = false;
-    //     state.error = action.payload;
-    //   })
+
+      //get employee by id
+      .addCase(getEmployeeById.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getEmployeeById.fulfilled, (state, action: PayloadAction<AxiosResponse<any>>) => {
+        state.loading = false;
+        state.selectedEmployee = action.payload.data;
+      })
+      .addCase(getEmployeeById.rejected, (state, action: PayloadAction<any>) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+
+      //get all employee
       .addCase(getAllEmployees.pending, (state) => {
         state.loading = true;
       })
@@ -71,21 +76,30 @@ const employeeSlice = createSlice({
         state.loading = false;
         state.error = action.payload;
       })
-    //   .addCase(updateEmployee.pending, (state) => {
-    //     state.loading = true;
-    //   })
-    //   .addCase(updateEmployee.fulfilled, (state, action: PayloadAction<AxiosResponse<Employee>>) => {
-    //     state.loading = false;
-    //     const updatedEmployee = action.payload.data;
-    //     state.employees = state.employees.map(employee =>
-    //       employee._id === updatedEmployee._id ? updatedEmployee : employee
-    //     );
-    //     state.selectedEmployee = updatedEmployee;
-    //   })
-    //   .addCase(updateEmployee.rejected, (state, action: PayloadAction<any>) => {
-    //     state.loading = false;
-    //     state.error = action.payload;
-    //   });
+
+      //update  employee by id
+      .addCase(updateEmployee.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(updateEmployee.fulfilled, (state, action: PayloadAction<AxiosResponse<any>>) => {
+        state.loading = false;
+        const updatedEmployee = action.payload?.data;
+      
+        if (updatedEmployee && Array.isArray(state.employees)) {
+          state.employees = state.employees.map(employee =>
+            employee?._id === updatedEmployee._id ? { ...employee, ...updatedEmployee } : employee
+          );
+        }
+      
+        if (updatedEmployee && state.selectedEmployee && state.selectedEmployee._id === updatedEmployee._id) {
+          state.selectedEmployee = { ...state.selectedEmployee, ...updatedEmployee };
+        }
+      })
+      
+      .addCase(updateEmployee.rejected, (state, action: PayloadAction<any>) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
   },
 });
 
