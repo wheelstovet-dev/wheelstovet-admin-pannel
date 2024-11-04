8dc3d45e0461d43402c4508a63dc0b7cb2b74841
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-11-04 15:00:19 +0530

    modified create admin mode

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,21 +1,21 @@
 import BreadCrumb from '@/components/breadcrumb';
-import { CreateAdminForm } from '@/components/forms/admin-stepper/create-admin';
-import { ComplaintForm } from '@/components/forms/complaint-stepper/create-complaint';
+import { AdminForm } from '@/components/forms/admin-stepper/create-admin';
+// import { ComplaintForm } from '@/components/forms/complaint-stepper/create-complaint';
 import MainLayout from '@/components/layout/main-layout';
 import { ScrollArea } from '@/components/ui/scroll-area';
-import { ComplaintManagementData } from '@/constants/complaint-management-data';
+// import { ComplaintManagementData } from '@/constants/complaint-management-data';
 
 const breadcrumbItems = [{ title: 'Employee', link: '/dashboard/employee' }];
 export default function page() {
   // Example structure of user options
 
   return (
-    <MainLayout meta={{ title: 'Employee Management' }}>
+    <MainLayout meta={{ title: 'Create Admin' }}>
 
     <ScrollArea className="h-full">
       <div className="flex-1 min-h-screen space-y-4 p-4 pt-6 md:p-8">
         <BreadCrumb items={breadcrumbItems} />
-        <CreateAdminForm  initialData={null} />
+        <AdminForm mode='create'  />
       </div>
     </ScrollArea>
     </MainLayout>
