91787a7bb8a61440521ac8bc9f158c24bcb615a3
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-12-09 10:51:44 +0530

    modified assignCases to employee

--- components/tables/assignedCases-table/client.tsx
+++ components/tables/assignedCases-table/client.tsx
@@ -1,18 +1,37 @@
 'use client';
 
-import { useState } from 'react';
-import { Button } from '@/components/ui/button';
+import { useEffect, useState } from 'react';
+// import { Button } from '@/components/ui/button';
 import { DataTable } from '@/components/ui/data-table';
 import { Heading } from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
-import {AssignedCase, AssignedCaseData} from '@/constants/assigned-case-data' ;
-import { Plus } from 'lucide-react';
-import { useRouter } from 'next/navigation';
+// import {AssignedCase, AssignedCaseData} from '@/constants/assigned-case-data' ;
+// import { Plus } from 'lucide-react';
+// import { useRouter } from 'next/navigation';
 import { columns } from './columns';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '@/app/redux/store';
+import { getAssignedCasesById } from '@/app/redux/actions/employeeAction';
 
 export const AssignedClient: React.FC = () => {
-  const router = useRouter();
-  const [data, setData] = useState<AssignedCase[]>(AssignedCaseData); 
+  // const router = useRouter();
+  // Parse the URL to get the 'id' parameter
+  const urlParams = new URLSearchParams(window.location.search);
+  const id = urlParams.get('id');
+
+  const dispatch = useDispatch<AppDispatch>();
+  const { assignedCases, loading } = useSelector((state: RootState) => state.employee);
+
+  useEffect(() => {
+    if (id) {
+      dispatch(getAssignedCasesById(id));
+    }
+  }, [dispatch, id]);
+
+  // console.log(assignedCases);
+
+  // const [initialCharges, setInitialCharges] = useState<RescueChargesFormValues>({} as RescueChargesFormValues);
+  // const [data, setData] = useState<InitialFormValues>({} as InitialFormValues);
 
 //   const handleSearch = (searchValue: string) => {
 //     const filteredData = TaskHistoryData.filter(item =>
@@ -46,21 +65,24 @@ export const AssignedClient: React.FC = () => {
     <>
       <div className="flex items-start justify-between">
         <Heading
-          title={`Assigned Cases (${data.length})`}
+          title={`Assigned Cases (${assignedCases.length})`}
           description="Manage case."
         />
       
       </div>
       <Separator />
+      {loading ? 'Loading...' :
+      
       <DataTable
         // searchKey="type"
         columns={columns}
-        data={data}
+        data={assignedCases}
         // onSearch={handleSearch}
         // filters={filters}
         // rowNo={0}
         // onSort={handleSort}
       />
+}
     </>
   );
 };
